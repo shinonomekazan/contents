ff046ae2bfe849f214c60a2f0b3eb881968bb260
Toyokazu Tsugehara <tsugehara@gmail.com>
2019-03-01 09:37:22 +0900

    add adsense

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,21 @@ module.exports = {
 	title: "東雲火山の山麓",
 	description: "株式会社東雲火山関連の記事等置き場",
 	theme: '@vuepress/default',
+	head: [
+		[
+			"script",
+			{src: "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+			async: true}
+		],
+		[
+			"script",
+			{},
+			`(adsbygoogle = window.adsbygoogle || []).push({
+google_ad_client: "ca-pub-5067037356613716",
+enable_page_level_ads: true
+});`,
+		]
+	],
 	themeConfig: {
 		nav: [
 			{ text: "リンク集", items: [
